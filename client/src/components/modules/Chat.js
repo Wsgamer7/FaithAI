b0109d449a1264e2bb97de5d91d2a5745e7fba99
Shuo Wang <ws18022@outlook.com>
2024-06-25 19:29:41 +0800

    auto scoll when a new message added

--- client/src/components/modules/Chat.js
+++ client/src/components/modules/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import SingleMessage from "./SingleMessage";
 import { NewMessage } from "./NewInput";
 import { get, post } from "../../utilities";
@@ -16,7 +16,7 @@ const msg1 = {
   content:
     "In CSS, when an element's width is set to 哈哈哈哈 a value greater than its parent's width, it can cause the element to overflow its parent container, potentially leading to layout issues and disrupting the visual appearance of the page. To handle such situations, you can employ various techniques to ensure a consistent and well-structured layout.",
 };
-const defalut_msgs = [msg0, msg1, msg1, msg0, msg1, msg1, msg1, msg1];
+const defalut_msgs = [msg0, msg1, msg1, msg0, msg1, msg1];
 
 /**
  * @param {string} userId
@@ -29,6 +29,7 @@ const Chat = ({ userId }) => {
    * @property {string} content
    */
   const [msgs, setMsgs] = useState([]);
+  const messagesEndRef = useRef(null);
   useEffect(() => {
     console.log("userId:" + userId);
     if (userId == undefined) {
@@ -42,6 +43,14 @@ const Chat = ({ userId }) => {
   const addNewMessage = (newMessage) => {
     setMsgs((prevMsgs) => [...prevMsgs, newMessage]);
   };
+  // Scroll to bottom when msgs change
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      duration: 500,
+    });
+  }, [msgs]);
 
   return (
     <div className="u-flexColumn Chat-warpper">
@@ -49,6 +58,7 @@ const Chat = ({ userId }) => {
         {msgs.map((msg, index) => (
           <SingleMessage key={index} userId={userId} message={msg} />
         ))}
+        <div ref={messagesEndRef} />
       </div>
       <div className="Chat-input ">
         <NewMessage userId={userId} addNewMessage={addNewMessage} />
